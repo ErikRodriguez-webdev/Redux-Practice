ef489ac2ccec2f0220ab27292f0ef7373873c5c7
Erik Rodriguez <eric19.21.03@gmail.com>
2020-02-20 17:34:54 -0800

    Add local state on playerform.js
    Used handleChange and handleSubmit functions to capture inputs and post
    players created to playersArray

--- project/src/components/PlayerForm.js
+++ project/src/components/PlayerForm.js
@@ -1,11 +1,68 @@
-import React from "react";
+import React, { useState } from "react";
+
+import { postPlayer } from "../actions/index";
 
 import { connect } from "react-redux";
+
+const [player, setPlayer] = useState({
+  name: "",
+  rank: "",
+  nickname: ""
+});
+
+const handleChanges = (event) => {
+  setPlayer({ [event.target.name]: event.target.value });
+};
+
+const handleSubmit = (event) => {
+  event.preventDefault();
+  props.postPlayer(player);
+  setPlayer({
+    name: "",
+    rank: "",
+    nickname: ""
+  });
+};
 
 const PlayerForm = (props) => {
   return (
     <div>
-      <p>playerform.js</p>
+      <form onSubmit={handleSubmit}>
+        <label>
+          Name:
+          <input
+            type="text"
+            name="name"
+            onChange={handleChanges}
+            value={player.name}
+            placeholder="Cristiano Ronaldo"
+          />
+        </label>
+
+        <label>
+          Rank:
+          <input
+            type="text"
+            name="rank"
+            onChange={handleChanges}
+            value={player.rank}
+            placeholder="2"
+          />
+        </label>
+
+        <label>
+          Nickname:
+          <input
+            type="text"
+            name="nickname"
+            onChange={handleChanges}
+            value={player.nickname}
+            placeholder="God"
+          />
+        </label>
+
+        <button type="submit">Submit</button>
+      </form>
     </div>
   );
 };
@@ -17,4 +74,4 @@ const mapStateToProps = (state) => {
   };
 };
 
-export default connect(mapStateToProps, {})(PlayerForm);
+export default connect(mapStateToProps, { postPlayer })(PlayerForm);
