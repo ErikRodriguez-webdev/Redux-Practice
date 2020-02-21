b84edebd32cc52727720e5f5eaa99d7c1992a38c
Erik Rodriguez <eric19.21.03@gmail.com>
2020-02-20 16:35:13 -0800

    Setup reducer and thunk on index.js
    Started store to globalize reducer to all components and add middleware
    with thunk

--- project/src/index.js
+++ project/src/index.js
@@ -1,10 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
 
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware } from "redux";
+import thunk from "redux-thunk";
+
+import { playerReducer } from "./reducers/index";
+
+const store = createStore(playerReducer, applyMiddleware(thunk));
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
---
+++ project/src/reducers/index.js
@@ -0,0 +1,12 @@
+export const initialState = {
+  playersArray: [],
+  isFetching: false,
+  errors: ""
+};
+
+export const playerReducer = (state = initialState, action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
