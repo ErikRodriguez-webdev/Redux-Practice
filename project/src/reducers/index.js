d65d3dd21edaaf3bdd1166e37ca4282989fb1b0c
Erik Rodriguez <eric19.21.03@gmail.com>
2020-02-20 18:41:12 -0800

    Add cases to reducer switch statement
    Added in all cases to reducer and imported actions strings to variables

--- project/src/reducers/index.js
+++ project/src/reducers/index.js
@@ -1,3 +1,12 @@
+import {
+  FETCH_DATA,
+  FETCH_SUCCESS,
+  FETCH_ERROR,
+  POST_DATA,
+  POST_SUCCESS,
+  POST_ERROR
+} from "../actions/index";
+
 export const initialState = {
   playersArray: [],
   isFetching: false,
@@ -7,6 +16,22 @@ export const initialState = {
 
 export const playerReducer = (state = initialState, action) => {
   switch (action.type) {
+    case FETCH_DATA:
+      return { ...state, isFetching: true };
+    case FETCH_SUCCESS:
+      return { ...state, playersArray: [action.payload], isFetching: false };
+    case FETCH_ERROR:
+      return { ...state, errors: [action.payload], isFetching: false };
+    case POST_DATA:
+      return { ...state, isPosting: true };
+    case POST_SUCCESS:
+      return {
+        ...state,
+        playersArray: [...state.playersArray, action.payload],
+        isPosting: false
+      };
+    case POST_ERROR:
+      return { ...state, errors: action.payload, isPosting: false };
     default:
       return state;
   }
